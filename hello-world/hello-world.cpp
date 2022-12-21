82b37d3c6a019196dcd010eb8d8efe0c089294ba
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2022-12-21 22:00:28 +0300

    Types

---
+++ hello-world/hello-world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int hello_world(){
+    printf("Hello World\r\n");
+    system("pause");
+    return 0;
+}
--- hello-world/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::printf("Hello World\r\n");
-    system("pause");
-    return 0;
-}
