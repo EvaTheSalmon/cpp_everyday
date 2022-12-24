567845b821f07e9f28a083f8e5962fea861415dd
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2022-12-24 22:50:02 +0300

    Strings

---
+++ strings/strings.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <cctype>
+#include <typeinfo>
+#include <iostream>
+
+using namespace std;
+bool isPalindrom(const string& str)
+{
+    for (int i = 0; i < (int)(str.size()); ++i) {
+        cout << typeid(str.size()-i).name() << endl;
+        if (tolower(str[i]) != tolower(str[str.size() - i - 1])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    if (isPalindrom("abba")) { cout << "1"; };
+    system("pause");
+    return 0;
+}
