7c4380b948f806169907964cdc15238a7feb24c0
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2022-12-27 01:26:24 +0300

    Another example

--- switch/switch.cpp
+++ switch/switch.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 //gcc case range https://gcc.gnu.org/onlinedocs/gcc-4.1.2/gcc/Case-Ranges.html
-char getGrade(int a, int b, int c) {
+char getGrade_switch(int a, int b, int c) {
 	int avg = (a + b + c) / 3;
 	switch (avg) {
 	case 90 ... 100: return 'A';
@@ -9,4 +9,19 @@ char getGrade(int a, int b, int c) {
 	case 60 ... 69: return 'D';
 	default: return 'F';
 	}
+}
+
+char getGrade(int a, int b, int c) {
+	auto score = (a + b + c) / 3;
+	if (score >= 90) return 'A';
+	if (score >= 80) return 'B';
+	if (score >= 70) return 'C';
+	if (score >= 60) return 'D';
+	return 'F';
+}
+
+int main() {
+	getGrade(70, 60, 100);
+	system("pause");
+	return 0;
 }
