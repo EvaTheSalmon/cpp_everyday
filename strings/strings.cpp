116ca43d2ff675ca7a2697db2dac28b474a589ad
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2022-12-29 17:07:44 +0300

    Update strings.cpp
    stoi

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -15,6 +15,17 @@ bool isPalindrom(const string& str)
     return true;
 }
 
+int points(const std::array<std::string, 10>& games) {
+  int sum = 0;
+  for (string str : games){
+    string x = str.substr(0, str.find(":"));
+    string y = str.substr(str.find(":")+1, str.length());
+    if (stoi(x)>stoi(y)) sum+=3;
+    if (stoi(x)==stoi(y)) sum+=1;
+  }  
+  return sum;
+}
+
 int master() {
     if (isPalindrom("abba")) { cout << "1"; };
     system("pause");
